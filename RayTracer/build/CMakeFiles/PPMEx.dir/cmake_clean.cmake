707b18ccb9c2809909afac0b2b2ffbd4a2a34643
Fernando Giardini <92555354+FernandoGiardini@users.noreply.github.com>
2024-04-02 00:35:01 -0300

    Add files via upload

---
+++ RayTracer/build/CMakeFiles/PPMEx.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PPMEx.dir/PPMEx.cpp.o"
+  "CMakeFiles/PPMEx.dir/PPMEx.cpp.o.d"
+  "PPMEx"
+  "PPMEx.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PPMEx.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ RayTracer/build/CMakeFiles/PPMEx.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for PPMEx.
+# This may be replaced when dependencies are built.
